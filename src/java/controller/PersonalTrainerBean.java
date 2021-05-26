20c0ab2af2b9fba80406885479ec588521ac93b0
YILMAZ ALPEREN <yilmzalpren@gmail.com>
2021-05-27 00:18:10 +0300

    PT sayfasinda filtreleme yapildi!

--- src/java/controller/PersonalTrainerBean.java
+++ src/java/controller/PersonalTrainerBean.java
@@ -10,18 +10,206 @@ import entity.PersonalTrainer ;
 
 import javax.faces.bean.ManagedBean ; 
 import javax.faces.bean.RequestScoped ; 
+import javax.faces.bean.SessionScoped ; 
 import java.util.ArrayList ; 
 
 
 @ManagedBean
 @RequestScoped
+
+
 public class PersonalTrainerBean {
     private ArrayList<PersonalTrainer> personalTrainerDao = null ;  // arraylist.
     private PersonalTrainerDAO personalTrainerDAO = null ;         // personaltrainer DAO object.
+     //  Cinsiyet
+    private static boolean female = false ;
+    private static boolean male = false ; 
+    //  Yas
+    private static boolean age25P = false ; 
+    private static boolean age30P = false ; 
+    private static boolean age35P = false ; 
+    private static boolean age40P = false ;
+    private static boolean age45P = false ; 
+    //  Egitim Durumu
+    private static boolean highLicense = false ; 
+    private static boolean license = false ;
+    //  Uzmanlık
+    private static boolean strong = false ;
+    private static boolean strength = false ; 
+    private static boolean sedanter = false ; 
+    private static boolean fitness = false ; 
+    //  Derecelendirme
+    private static boolean star5 = false ; 
+    private static boolean star4 = false ; 
+    private static boolean star3 = false ; 
+    private static boolean star2 = false ; 
+    private static boolean star1 = false ; 
+
+    
+    public PersonalTrainerDAO getPersonalTrainerDAO() {
+        return personalTrainerDAO;
+    }
+
+    public void setPersonalTrainerDAO(PersonalTrainerDAO personalTrainerDAO) {
+        this.personalTrainerDAO = personalTrainerDAO;
+    }
+
+    public boolean isFemale() {
+        return female;
+    }
+
+    public void setFemale(boolean female) {
+        this.female = female;
+    }
+
+    public boolean isMale() {
+        return male;
+    }
+
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+
+    public boolean isAge25P() {
+        return age25P;
+    }
+
+    public void setAge25P(boolean age25P) {
+        this.age25P = age25P;
+    }
+
+    public boolean isAge30P() {
+        return age30P;
+    }
+
+    public void setAge30P(boolean age30P) {
+        this.age30P = age30P;
+    }
+
+    public boolean isAge35P() {
+        return age35P;
+    }
+
+    public void setAge35P(boolean age35P) {
+        this.age35P = age35P;
+    }
+
+    public boolean isAge40P() {
+        return age40P;
+    }
+
+    public void setAge40P(boolean age40P) {
+        this.age40P = age40P;
+    }
+
+    public boolean isAge45P() {
+        return age45P;
+    }
+
+    public void setAge45P(boolean age45P) {
+        this.age45P = age45P;
+    }
+
+    public boolean isHighLicense() {
+        return highLicense;
+    }
+
+    public void setHighLicense(boolean highLicense) {
+        this.highLicense = highLicense;
+    }
+
+    public boolean isLicense() {
+        return license;
+    }
+
+    public void setLicense(boolean license) {
+        this.license = license;
+    }
+
+    public boolean isStrong() {
+        return strong;
+    }
+
+    public void setStrong(boolean strong) {
+        this.strong = strong;
+    }
+
+    public boolean isStrength() {
+        return strength;
+    }
+
+    public void setStrength(boolean strength) {
+        this.strength = strength;
+    }
+
+    public boolean isSedanter() {
+        return sedanter;
+    }
+
+    public void setSedanter(boolean sedanter) {
+        this.sedanter = sedanter;
+    }
+
+    public boolean isFitness() {
+        return fitness;
+    }
+
+    public void setFitness(boolean fitness) {
+        this.fitness = fitness;
+    }
+
+    public boolean isStar5() {
+        return star5;
+    }
+
+    public void setStar5(boolean star5) {
+        this.star5 = star5;
+    }
+
+    public boolean isStar4() {
+        return star4;
+    }
+
+    public void setStar4(boolean star4) {
+        this.star4 = star4;
+    }
+
+    public boolean isStar3() {
+        return star3;
+    }
+
+    public void setStar3(boolean star3) {
+        this.star3 = star3;
+    }
+
+    public boolean isStar2() {
+        return star2;
+    }
+
+    public void setStar2(boolean star2) {
+        this.star2 = star2;
+    }
+
+    public boolean isStar1() {
+        return star1;
+    }
+
+    public void setStar1(boolean star1) {
+        this.star1 = star1;
+    }
+
+   
+
+    public String filtreleme(){
+        // to do something.
+        return null ; 
+    }
 
     public ArrayList<PersonalTrainer> getPersonalTrainerDao() {
         personalTrainerDAO = new PersonalTrainerDAO();
-        personalTrainerDao =personalTrainerDAO.getPersonalTrainerList(); 
+        personalTrainerDao =personalTrainerDAO.getPersonalTrainerList(isFemale(),isMale(),isAge25P(),isAge30P(),isAge35P(),
+                isAge40P(),isAge45P(),isHighLicense(),isLicense(),isStrong(),isStrength(),isSedanter(),isFitness(),isStar5(),isStar4(),
+                isStar3(),isStar2(),isStar1()); 
         return personalTrainerDao;
     }
     
